25b70b7c95211cea59bf81c84b33206f2dd0135e
Kent Tsou <tsoukent97@gmail.com>
2021-04-19 00:36:29 +1200

    initial initial commit

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,6 +1,15 @@
 import request from 'superagent'
 
+const serverURL = 'http://localhost:3000/api/v1'
+
 export function getGreeting() {
-  return request.get('/greeting')
-                .then(res => res.body.greeting)
+  return request
+  .get(`${serverURL}/greeting`)
+  .then(res => res.body)
+}
+
+export function nbaChamps() {
+  return request 
+  .get('https://fly.sportsdata.io/api/nba/fantasy/json/CurrentSeason')
+  .then(res => res.body)
 }
---
+++ client/components/NbaStats.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NbaStats() {
+    return (
+        <div></div>
+    )
+}
+
+export default NbaStats
